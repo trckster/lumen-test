98d28b3555a10202b89ee9497050fa3a0b8985cd
Daniil <dan-trofimov@mail.ru>
2020-07-31 19:26:33 +0300

    Add code lifetime check

--- app/Services/EmailVerificationService.php
+++ app/Services/EmailVerificationService.php
@@ -3,19 +3,40 @@
 namespace App\Services;
 
 use App\Mail\VerificationCode;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Mail;
 
 class EmailVerificationService
 {
+    const CODE_LIFE_TIME = 5 * 60;
+
+    const CODE_SENT_KEY = 'code-sent-';
+
     private function generateCode(): string
     {
         return strval(rand(1000, 9999));
     }
 
+    private function codeAlreadySent(string $email): bool
+    {
+        return Cache::has(self::CODE_SENT_KEY . $email);
+    }
+
+    private function processCodeSendingAftermath(string $email): void
+    {
+        Cache::put(self::CODE_SENT_KEY . $email, true, self::CODE_LIFE_TIME);
+    }
+
     public function sendVerificationEmail(string $email): void
     {
+        if ($this->codeAlreadySent($email)) {
+            abort(429,'code_has_been_already_sent');
+        }
+
         $code = $this->generateCode();
 
         Mail::to($email)->queue(new VerificationCode($code));
+
+        $this->processCodeSendingAftermath($email);
     }
 }
--- tests/Feature/EmailVerificationTest.php
+++ tests/Feature/EmailVerificationTest.php
@@ -3,11 +3,30 @@
 namespace Feature;
 
 use App\Mail\VerificationCode;
+use App\Services\EmailVerificationService;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Mail;
 use TestCase;
 
 class EmailVerificationTest extends TestCase
 {
+    private function mockCacheForCodeSending(string $email): void
+    {
+        Cache::shouldReceive('has')
+            ->once()
+            ->with(EmailVerificationService::CODE_SENT_KEY . $email)
+            ->andReturn(false);
+
+        Cache::shouldReceive('put')
+            ->once()
+            ->with(
+                EmailVerificationService::CODE_SENT_KEY . $email,
+                true,
+                EmailVerificationService::CODE_LIFE_TIME
+            );
+
+    }
+
     /**
      * @test
      */
@@ -17,6 +36,8 @@ class EmailVerificationTest extends TestCase
 
         $email = 'my@mail.ru';
 
+        $this->mockCacheForCodeSending($email);
+
         $this->json('GET','sendCode', ['email' => $email])
             ->seeJson([
                 'status' => 'success'
@@ -25,5 +46,31 @@ class EmailVerificationTest extends TestCase
         Mail::assertQueued(VerificationCode::class, function ($mail) use ($email) {
             return $mail->hasTo($email);
         });
+    }
+
+    /**
+     * @test
+     */
+    public function cant_send_code_twice()
+    {
+        Mail::fake();
+
+        $email = 'my@mail.ru';
+
+        $this->mockCacheForCodeSending($email);
+
+        $this->json('GET','sendCode', ['email' => 'my@mail.ru'])
+            ->seeJson([
+                'status' => 'success'
+            ]);
+
+        Cache::shouldReceive('has')
+            ->once()
+            ->with(EmailVerificationService::CODE_SENT_KEY . $email)
+            ->andReturn(true);
+
+        $this->json('GET', 'sendCode', ['email' => 'my@mail.ru'])
+            ->seeStatusCode(429)
+            ->seeJson(['code_has_been_already_sent']);
     }
 }
